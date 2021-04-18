d771c1406fc6dafe72d3faeeae642ed41cc9a48c
Himanshu <Himanshu.shivhare.9935@gmail.com>
2021-04-18 16:11:27 +0530

    priceCompare Activity Completed!

--- activity/priceCompare.js
+++ activity/priceCompare.js
@@ -13,8 +13,8 @@ console.log("Before");
             args:["--start-maximized"]
         });
         getListingFromAmazon(links[0] , browserInstance , pName);
-
-
+        getListingFromFlipkart(links[1] , browserInstance , pName);
+        getListingFromPaytmMall(links[2] , browserInstance , pName);
 
     }catch(err){
         console.log(err);
@@ -28,9 +28,6 @@ async function getListingFromAmazon(link , browserIntance , pName){
     await newTab.goto(link);
     await newTab.type("#twotabsearchtextbox" , pName , {delay: 200});
     await newTab.click("#nav-search-submit-button");
-
-    let productArr=[];
-    let priceArr=[];
     await newTab.waitForSelector(".sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20 h2" , {visible:true} );
     
     function browserRunConsoleFn(productSelector , priceSelector){
@@ -48,12 +45,62 @@ async function getListingFromAmazon(link , browserIntance , pName){
     }
 
    let details= await newTab.evaluate(browserRunConsoleFn,".sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20 h2" ,".a-price-whole");
-   console.log(details);
-
+   console.table(details);
 
 }
 
-async function waitAndClick(selector , newTab){
-    await newTab.waitForSelecto(selector , {visible : true});
-    return newTab.click(selector);
+async function getListingFromFlipkart(link , browserInstance , pName){
+    let newTab=await browserInstance.newPage();
+    await newTab.goto(link);
+    await newTab.type("._3704LK" , pName , {delay:200});
+    await newTab.click(".L0Z3Pu");
+    await newTab.click(".L0Z3Pu");
+    await newTab.waitForSelector("._4rR01T" , {visible:true});
+
+    function consoleRunFn(productSelector , priceSelector){
+        let allProducts=document.querySelectorAll(productSelector);
+        let allPrices=document.querySelectorAll(priceSelector);
+        let details=[];
+
+        for(let i=0;i<5;i++){
+            let oneProduct=allProducts[i].innerText;
+            let onePrice=allPrices[i].innerText;
+            details.push([
+                oneProduct , onePrice
+            ])
+        }
+        return details;
+    }
+    let details=await newTab.evaluate(consoleRunFn , "._4rR01T" , "._30jeq3._1_WHN1");
+    console.log("```````````````````````````````````````````````````");
+    console.table(details)       
+
+}
+
+
+async function getListingFromPaytmMall(link , browserInstance , pName){
+    let newTab=await browserInstance.newPage();
+    await newTab.goto(link);
+    await newTab.type("#searchInput" , pName , {delay:200});
+    await newTab.keyboard.press("Enter" , {delay:200});
+    await newTab.waitForSelector(".UGUy" , {visible:true});
+
+    function consoleRunFn(productSelector , priceSelector){
+        let allProducts=document.querySelectorAll(productSelector);
+        let allPrices=document.querySelectorAll(priceSelector);
+        let details=[];
+
+        for(let i=1;i<6;i++){
+            let oneProduct=allProducts[i].innerText;
+            let onePrice=allPrices[i].innerText;
+            details.push([
+                oneProduct , onePrice
+            ])
+        }
+        return details;
+    }
+    let details=await newTab.evaluate(consoleRunFn , ".UGUy" , "._1kMS");
+    console.log("------PayTM-----------Compare-------");
+    console.table(details)       
+
 }
